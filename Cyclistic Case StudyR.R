8f4756504ac1ad8fea1735f6ae88d1305386cc4f
ethompson17 <47339937+ethompson17@users.noreply.github.com>
2022-03-24 18:40:03 -0400

    Add files via upload

---
+++ Cyclistic Case StudyR.R
@@ -0,0 +1,94 @@
+#load packages
+library("tidyverse")
+library("janitor")
+library("lubridate")
+
+#set working directory pc
+setwd("C:/Users/ethom/Desktop/Data Analytics/Cyclistic/Dataset Files (Cyclistic)")
+
+
+#load dataset
+
+Mar21Data <- read.csv("202103-divvy-tripdata.csv")
+Apr21Data <- read.csv("202104-divvy-tripdata.csv")
+May21Data <- read.csv("202105-divvy-tripdata.csv")
+Jun21Data <- read.csv("202106-divvy-tripdata.csv")
+Jul21Data <- read.csv("202107-divvy-tripdata.csv")
+Aug21Data <- read.csv("202108-divvy-tripdata.csv")
+Sep21Data <- read.csv("202109-divvy-tripdata.csv")
+Oct21Data <- read.csv("202110-divvy-tripdata.csv")
+Nov21Data <- read.csv("202111-divvy-tripdata.csv")
+Dec21Data <- read.csv("202112-divvy-tripdata.csv")
+Jan22Data <- read.csv("202201-divvy-tripdata.csv")
+Feb22Data <- read.csv("202202-divvy-tripdata.csv")
+
+
+#combine into one dataframe
+df <- rbind(Mar21Data,Apr21Data,May21Data,Jun21Data,Jul21Data,Aug21Data,Sep21Data,Oct21Data,Nov21Data,Dec21Data,Jan22Data,Feb22Data)
+
+
+
+
+
+#data preview
+head(df)
+
+glimpse(df)
+
+str(df)
+
+colnames(df)
+
+
+df$started_at = as_datetime(df$started_at, tz = "America/Chicago")
+class(df$started_at)
+
+df$ended_at = as_datetime(df$ended_at, tz = "America/Chicago")
+class(df$ended_at)
+
+
+
+df$ridelength <- difftime(df$ended_at,df$started_at, units = "mins")
+
+
+---------------------------------
+
+#all casual/members
+casualriders <- df %>% filter(member_casual == "casual")
+memberriders <- df %>% filter(member_casual == "member")
+
+
+
+#Average Ride Length
+
+df$started_at = as_datetime(df$started_at, tz = "America/Chicago")
+class(df$started_at)
+
+df$ended_at = as_datetime(df$ended_at, tz = "America/Chicago")
+class(df$ended_at)
+
+df$ridelength <- difftime(df$ended_at,df$started_at, units = "mins")
+
+df$ridelength <- as.period(ridelength)
+
+df <- df %>% filter(ridelength > 0)
+
+#To Check if there are Negative Ride Lengths (`started_at` is later than `ended_at`)
+
+df %>% filter(ridelength < 0)
+
+df %>% filter(member_casual == "casual") %>% summarise(mean(ridelength))
+
+df %>% filter(member_casual == "member") %>% summarise(mean(ridelength))
+
+df %>% filter(ridelength > 20)
+
+avg_ridelength_casual <- df %>% filter(member_casual == "casual")
+
+avg_ridelength_member <- df %>% filter(member_casual == "member")
+
+ggplot()+
+  geom_boxplot(mapping = aes(x = avg_ridelength_casual,avg_ridelength_member))
+
+qplot(x = avg_ridelength_casual, avg_ridelength_member)
+
